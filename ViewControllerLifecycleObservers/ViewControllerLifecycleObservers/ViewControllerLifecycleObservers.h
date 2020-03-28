d1953a98c28c8788e650b23275c0b7e80d72daf2
thinhle <thinhlefrs@gmail.com>
2020-03-28 13:36:53 +0700

    init view controller life cycle observers

---
+++ ViewControllerLifecycleObservers/ViewControllerLifecycleObservers/ViewControllerLifecycleObservers.h
@@ -0,0 +1,19 @@
+//
+//  ViewControllerLifecycleObservers.h
+//  ViewControllerLifecycleObservers
+//
+//  Created by ThinhLe on 3/28/20.
+//  Copyright © 2020 ThinhLe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ViewControllerLifecycleObservers.
+FOUNDATION_EXPORT double ViewControllerLifecycleObserversVersionNumber;
+
+//! Project version string for ViewControllerLifecycleObservers.
+FOUNDATION_EXPORT const unsigned char ViewControllerLifecycleObserversVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewControllerLifecycleObservers/PublicHeader.h>
+
+
